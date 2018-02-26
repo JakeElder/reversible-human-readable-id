dc64a8bc858a1334dbfd39ace997e86dfbffb1f8
spiritai_david <david@spiritai.com>
2018-02-26 17:52:59 +0000

    Updated hasher lib
    Signed-off-by: spiritai_david <david@spiritai.com>

--- index.js
+++ index.js
@@ -2,23 +2,11 @@ const animals = require('./assets/animals');
 const adjectives = require('./assets/adjectives');
 const scientists = require('./assets/scientists');
 const local = require('./local.js');
-const hasher = require('node-cityhash');
+const hasher = require('cityhash-js');
 
 // Helpers
 function parser(stringNumber) {
-  // Dashes seem to be present in hash lib
-  // I am going to shift the number by a factor of 10 or 11 given the position
-  // This means that the name arrays length needs to be at least 119
-  if (stringNumber.indexOf('-') > -1) {
-    const indexDash = stringNumber.indexOf('-');
-    const indexOfOther = indexDash ^ 1;
-    const newStringNumber = String(10 + indexDash) + stringNumber[indexOfOther];
-    return Number(newStringNumber);
-  }
-  if (Number(stringNumber)) {
-    return Number(stringNumber);
-  }
-  return NaN;
+  return Number(stringNumber);
 }
 
 function generateHR(hash) {
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,14 +3,14 @@ const { getHR, getId } = require('../index.js');
 describe('cityhash', () => {
   describe('getHR', () => {
     it('should return a human readable name', () => {
-      expect(getHR('9028j9ko39i53klf')).toBe('swift-engelbart-grasshopper');
-      expect(getHR('34400394682')).toBe('brown-archimedes-kangaroo');
+      expect(getHR('9028j9ko39i53klf')).toBe('massive-bartik-husky');
+      expect(getHR('34400394682')).toBe('massive-lichterman-sheep');
     });
   });
   describe('getId', () => {
     it('should return an id from a stored human readable name', () => {
-      getHR('9028j9ko39i53klf');
-      expect(getId('swift-engelbart-grasshopper')).toBe('9028j9ko39i53klf');
+      const id = '9028j9ko39i53klf';
+      expect(getId(getHR(id))).toBe(id);
     });
   });
 });
