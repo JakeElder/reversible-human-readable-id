5dca0518523c15130a6d48b9f9947c644871f0fa
dark0dave <dark0dave@mykolab.com>
2018-04-12 20:03:38 +0100

    Added more assets, such that we can have more combinations and version bump
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const animals = require('./assets/animals');
 const adjectives = require('./assets/adjectives');
 const scientists = require('./assets/scientists');
+const colors = require('./assets/colors.js');
 const local = require('./local.js');
 const shajs = require('sha.js');
 
@@ -10,12 +10,11 @@ function parser(stringNumber) {
 }
 
 function generateHR(hash) {
-  const adjective = adjectives[parser(hash.slice(1, 2))];
-  const animal = animals[parser(hash.slice(2, 3))];
-  const scientist = scientists[parser(hash.slice(3, 4))];
+  const adjective = adjectives[parser(hash.slice(1, 3)) * 2];
+  const color = colors[parser(hash.slice(3, 5)) * 2];
+  const scientist = scientists[parser(hash.slice(5, 7))];
 
-  const number = parser(hash.slice(7, 8));
-  const arr = [adjective, scientist, animal, number].filter(x => x);
+  const arr = [adjective, scientist, color].filter(x => x);
 
   return arr.join('-');
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -4,7 +4,7 @@ describe('cityhash', () => {
   describe('getHR', () => {
     it('should return a human readable name', () => {
       const id = '9028j9ko39i53klf';
-      expect(getHR(id)).toBe('chatty-albattani-bulldog-13');
+      expect(getHR(id)).toBe('bacterial-easley-alizarin-crimson');
     });
     it('should always return the same human readable name', () => {
       const id = '9028j9ko39i53klf';
